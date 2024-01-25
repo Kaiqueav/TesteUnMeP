95fcbc88f62a65fb4fcdbf82b26dc305e6a73b6f
Kaiqueav <kaiquecerq66@outlook.com>
2024-01-24 22:30:25 -0300

    feat: add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+      <div className="p-2  bg-blue-950">
+          <p className="Title text-sky-700 font-bold text-2xl ml-8">UnMEP <b className="text-white ml-5"> To-Do</b> </p>
+      </div>
+    )
+  }
