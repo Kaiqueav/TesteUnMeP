5b99010d16498f7f23ba746d94dac4c5313df887
Kaiqueav <kaiquecerq66@outlook.com>
2024-01-24 23:42:17 -0300

    att

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Task } from "./task/Task";
+import { Task } from "../task/Task";
 export const Form = () => {
     
     const [todos, setTodos] = useState([]);
